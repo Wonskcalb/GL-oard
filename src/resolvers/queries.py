48e62177e7a2b721e61cd3247b56aad3f6eb03aa
Valentin Famelart <valentin.famelart@invenis.co>
2021-03-21 18:29:04 +0100

    feat(projects): Add get projects list
    - Add types file to store GQL types,
    - Add Query to fetch existing projects on the remote
    - Remove useless queries

--- src/resolvers/queries.py
+++ src/resolvers/queries.py
@@ -1,9 +1,22 @@
+import logging
 from typing import Any, Dict, List, Optional
 
+import gitlab
+from gitlab.exceptions import GitlabAuthenticationError
 from tartiflette import Resolver
 
+from src.globals import GITLAB_HOST, GITLAB_TOKEN
 
-@Resolver("Query.hello")
-async def hello(parent, args, context, info):
-    name = args["name"]
-    return f"Hello, {name}!"
+logger = logging.getLogger(__name__)
+
+
+@Resolver("Query.projects")
+async def projects(parent, args, context, info):
+    gl = gitlab.Gitlab(GITLAB_HOST, private_token=GITLAB_TOKEN)
+
+    try:
+        gl.auth()
+    except GitlabAuthenticationError as e:
+        logging.error("Could not connect user")
+
+    return gl.projects.list(archived=args.get("archived", False))
